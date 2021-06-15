5f89e8ee4675907ea23ce4c0cb920e1f37760b6a
victor-singha <vicunite2@gmail.com>
2021-06-15 15:18:06 +0530

    [victor] POST request add data to DB

--- RepositoryLayer/Services/UserRL.cs
+++ RepositoryLayer/Services/UserRL.cs
@@ -8,11 +8,17 @@ namespace RepositoryLayer.Services
 {
     public class UserRL : IUserRL
     {
+        FundooContext fundooContext;
+        public UserRL(FundooContext fundooContext)
+        {
+            this.fundooContext = fundooContext;
+        }
         public void RegisterUser(User user)
         {
             try
             {
-
+                fundooContext.Users.Add(user);
+                fundooContext.SaveChanges();
             }
             catch(Exception e)
             {

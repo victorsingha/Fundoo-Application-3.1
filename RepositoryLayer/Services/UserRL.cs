e31f6e90ec73b3b4d7d7983197a06d5dbc44bab6
victor-singha <vicunite2@gmail.com>
2021-06-16 19:57:36 +0530

    [victor] Authenticate User in database

--- RepositoryLayer/Services/UserRL.cs
+++ RepositoryLayer/Services/UserRL.cs
@@ -4,6 +4,7 @@ using RepositoryLayer.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 
@@ -19,26 +20,36 @@ namespace RepositoryLayer.Services
 
         public string AuthenticateUser(string email, string password)
         {
-            if(email != "john@gmail.com" || password != "12345")
+            try
             {
-                return null;
-            }
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var tokenKey = Encoding.ASCII.GetBytes("THIS_IS_MY_KEY_TO_GENERATE_TOKEN");
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
+                var result = _fundooContext.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+                if (result == null)
                 {
+                    return null;
+                }
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenKey = Encoding.ASCII.GetBytes("THIS_IS_MY_KEY_TO_GENERATE_TOKEN");
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
                     new Claim(ClaimTypes.Email,email)
-                }),
-                Expires = DateTime.UtcNow.AddHours(1),
-                SigningCredentials =
-                new SigningCredentials(
-                    new SymmetricSecurityKey(tokenKey),
-                    SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
+                    }),
+                    Expires = DateTime.UtcNow.AddHours(1),
+                    SigningCredentials =
+                    new SigningCredentials(
+                        new SymmetricSecurityKey(tokenKey),
+                        SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return tokenHandler.WriteToken(token);
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            
+
         }
 
         public void RegisterUser(User user)

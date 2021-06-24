b5319e0f72220261521013fa727bf6506889ab4b
victor-singha <vicunite2@gmail.com>
2021-06-24 14:55:57 +0530

    authorize

--- FundooApplication/Controllers/NotesController.cs
+++ FundooApplication/Controllers/NotesController.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace FundooApplication.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class NotesController : ControllerBase

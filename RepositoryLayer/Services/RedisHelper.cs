3a185499f2d656539ba54ae0ef6e683340baa9e8
victor-singha <vicunite2@gmail.com>
2021-06-25 16:35:19 +0530

    [victor] redis cache

---
+++ RepositoryLayer/Services/RedisHelper.cs
@@ -0,0 +1,29 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class RedisHelper
+    {
+        public static void SaveData(string host,string key,string value)
+        {
+            using (RedisClient client = new RedisClient(host))
+            {
+                if(client.Get<string>(key) == null)
+                {
+                    client.Set(key, value);
+                }
+            }
+        }
+
+        public static string ReadData(string host,string key)
+        {
+            using (RedisClient client = new RedisClient(host))
+            {
+                return client.Get<string>(key);
+            }
+        }
+    }
+}
